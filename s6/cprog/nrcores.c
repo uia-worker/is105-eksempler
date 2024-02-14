4bd0cc466e8a72a00c3e0b93ac478bfd31d26172
Janis Gailis <janis.gailis@uia.no>
2024-02-14 20:46:12 +0000

    la til main i nrcores.c

--- s6/cprog/nrcores.c
+++ s6/cprog/nrcores.c
@@ -6,7 +6,8 @@
 #else
 #include <unistd.h>
 #endif
- 
+#include <stdio.h>
+
 int getNumberOfCores() {
 #ifdef WIN32
     SYSTEM_INFO sysinfo;
@@ -16,10 +17,10 @@ int getNumberOfCores() {
     int nm[2];
     size_t len = 4;
     uint32_t count;
- 
+
     nm[0] = CTL_HW; nm[1] = HW_AVAILCPU;
     sysctl(nm, 2, &count, &len, NULL, 0);
- 
+
     if(count < 1) {
     nm[1] = HW_NCPU;
     sysctl(nm, 2, &count, &len, NULL, 0);
@@ -29,4 +30,11 @@ int getNumberOfCores() {
 #else
     return sysconf(_SC_NPROCESSORS_ONLN);
 #endif
+}
+
+// Hovedprogram
+int main() {
+    printf("Number of cores: %d\n", getNumberOfCores());
+
+    return 0;
 }
