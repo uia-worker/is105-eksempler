3d68d4e5324aa48e789f4eebea3bb6c7985efbe5
Janis Gailis <janis.gailis@uia.no>
2024-02-21 12:50:05 +0000

    ackerman

---
+++ s7/cprog/ack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+static int ackermann(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (n == 0) {
+        return ackermann(m - 1, 1);
+    } else {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+}
+
+int main() {
+    printf("%d\n", ackermann(5,1));
+    return 0;
+}
+
