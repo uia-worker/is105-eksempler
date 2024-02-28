719c3317ca67e33c5e477f7ce164e4e66fc78dde
Janis Gailis <janis.gailis@uia.no>
2024-02-28 14:18:23 +0100

    s8

---
+++ s8/csharp/hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Project_1 {
+    class MainClass {
+        public static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+            Console.ReadKey ();
+        }
+    }
+}
